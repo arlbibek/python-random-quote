0a0bd7f927e92cb715aad2d55bf84eba4bd77d81
arlbibek <53701945+arlbibek@users.noreply.github.com>
2020-08-02 19:58:08 +0545

    Updated

--- get-quote.py
+++ get-quote.py
@@ -4,9 +4,8 @@ import random
 def main():
     with open("quotes.txt", "rt", encoding='UTF8') as f:
         quotes = f.readlines()
-
-    random_int = random.randint(0, len(quotes))
-    print(quotes[random_int])
+    random_int = random.randint(0, len(quotes) - 1)
+    print(quotes[random_int].strip())
 
 
 if __name__ == "__main__":
